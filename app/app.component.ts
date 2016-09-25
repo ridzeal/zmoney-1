d257ad16f67089791e064e71871f9181fd757b3a
Riadhi Aditya <ridzeal@gmail.com>
2016-09-25 18:55:14 +0700

    Modify based on Tour of Heroes Tutorial - The Hero Editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{account.name}} Account</h2>
+        <div><label>id: </label>{{account.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="account.name" placeholder="name">
+        </div>
+        `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'zMoney';
+    account: Account = {
+        id: 1,
+        name: 'Personal'
+    };
+}
+
+export class Account {
+    id: number;
+    name: string;
+}
